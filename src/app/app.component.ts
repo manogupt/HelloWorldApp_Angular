9103c9cc419c4ddc25b88351b2ec93849c3690b2
manogupt <manojgupta123929@gmail.com>
2022-09-08 13:42:35 +0530

    Added : Added bridgelabz logo using property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imgUrl="./assets/logo.jpg";
+
+  ngOnInit():void{
+    this.title ="Hello From Bridgelabz."
+    
+    
+    
+
+  }
 }
